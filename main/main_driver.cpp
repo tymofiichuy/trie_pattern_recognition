c8dc6cd12d05542ed06c00797fbde3745de6b23b
tymofiichuy <tymofiichuy@gmail.com>
2025-04-30 13:26:03 +0300

    Add time measurement

--- main/main_driver.cpp
+++ main/main_driver.cpp
@@ -7,10 +7,23 @@ int main(){
     pattern_recognition ptr;
     ptr.generate_input("input.dat");
 
+    chrono::milliseconds trie_generation(0);
+    chrono::milliseconds recognition(0);
+    chrono::steady_clock::time_point start;
+    chrono::steady_clock::time_point end;
+
     ifstream in("input.dat");
     ofstream out("output.dat");
+
+    start = chrono::high_resolution_clock::now();
     ptr.read_line(in);
+    end = chrono::high_resolution_clock::now();
+    cout << "Trie generation time: " << chrono::duration_cast<chrono::milliseconds>(end-start).count() << " ms\n";
+
+    start = chrono::high_resolution_clock::now();
     ptr.find_patterns(in, out);
+    end = chrono::high_resolution_clock::now();
+    cout << "Pattern recognition time: " << chrono::duration_cast<chrono::milliseconds>(end-start).count() << " ms";
 
     return 0;
 }
